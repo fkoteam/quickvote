10f45030804005348546d13f9106fec166dc695a
fkoteam <agl_2002@yahoo.es>
2025-11-22 17:54:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teatro interactivo</title>
+    <title>Tus respuestas marcarán el camino</title>
     <style>
         * {
             margin: 0;
