51d9f32b3da06874ba5992b46593dd2e5f748c0a
fkoteam <agl_2002@yahoo.es>
2025-11-22 17:57:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tus respuestas marcarn el camino</title>
+    <title>Tus respuestas marcarán el camino</title>
     <style>
         * {
             margin: 0;
